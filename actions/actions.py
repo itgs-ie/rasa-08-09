340a48e3200b3dbe51fe4b16585b36d6b50d647d
Muhammad Danish <worldac2017@gmail.com>
2021-08-05 11:49:40 +0500

    send email

--- actions/actions.py
+++ actions/actions.py
@@ -1,3 +1,76 @@
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+
+        SendEmail(
+            tracker.get_slot("email"),
+            tracker.get_slot("subject"),
+            tracker.get_slot("message")
+        )
+        dispatcher.utter_message("Thanks for providing the details. We have sent you a mail at {}".format(tracker.get_slot("email")))
+        return []
+
+def SendEmail(toaddr,subject,message):
+    fromaddr = "worldac2017@gmail.com"
+
+    msg = MIMEMultipart()
+
+
+    msg['From'] = fromaddr
+
+
+    msg['To'] = toaddr
+
+
+    msg['Subject'] = subject
+
+
+    body = message
+
+
+    msg.attach(MIMEText(body, 'plain'))
+
+
+
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+
+    s.starttls()
+
+
+
+    try:
+        s.login(fromaddr, "ustad5042141ZMEE&&H&")
+
+       
+        text = msg.as_string()
+
+     
+        s.sendmail(fromaddr, toaddr, text)
+    except Exception as e:
+        print("An Error occured while sending email." + str(e))
+    finally:
+      
+        s.quit()
+
 # This files contains your custom actions which can be used to run
 # custom Python code.
 #
